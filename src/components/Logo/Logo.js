8dadc745ce85ff0f41d43752dfd6c1f4a113af91
Jona Reyes <jonamreyes@gmail.com>
2018-08-16 20:53:22 -0400

    added logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Tilt from 'react-tilt';
+import brain from './brain.png';
+import './Logo.css';
 
 const Logo = () => {
 	return (
 		<div className= 'ma4 mt0'>
-			<Tilt className="Tilt" options={{ max : 25 }} style={{ height: 150, width: 150 }} >
- 				<div className="Tilt-inner"> 👽 </div>
+			<Tilt className="Tilt br2 shadow-2" options={{ max : 25 }} style={{ height: 150, width: 150 }} >
+ 				<div className="Tilt-inner"> 
+ 					<img style={{paddingTop: '5px'}} src={brain} alt={'logo'}/> 
+ 				</div>
 			</Tilt>
 		</div>
 
