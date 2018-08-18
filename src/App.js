ea78e145d78af5342ebc5b54d958b00dc44e4392
Jona Reyes <jonamreyes@gmail.com>
2018-08-18 12:03:58 -0400

    added face recognition function - face box

--- src/App.js
+++ src/App.js
@@ -34,10 +34,33 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '', 
+      box: {}
     }
   }
   
+  //calculate face location based on the input
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    console.log(width, height);
+
+    //this will fill the box state
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box});
+  }
+
   //event listerners with states
   onInputChange = (event) => {
     this.setState({input: event.target.value});
@@ -46,20 +69,12 @@ class App extends Component {
     //then pass this on to the facerecognition
     this.setState ({imageUrl: this.state.input});
     
-    // CLARIFAI API 
-    app.models.predict(
-      Clarifai.FACE_DETECT_MODEL, 
+  // CLARIFAI API 
+  app.models.predict(
+    Clarifai.FACE_DETECT_MODEL, 
       this.state.input)
-    .then(
-    function(response) {
-        // do something with response
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-      },
-      function(err) {
-       // there was an error
-       console.log(err);
-      }
-    );
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(err => console.log(err));
   }
 
   render() {
@@ -80,10 +95,11 @@ class App extends Component {
         { 
         /*add face recognition function using Clarifai Face Recognition API 
           display the image under the form
+          display face box
         */
         }
 
-        <FaceRecognition imageUrl={this.state.imageUrl}/>
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
         
         
       </div>
