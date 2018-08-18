1ddf72a83a6cef1eeb1d4fc59cef37ad3ffaa29c
Jona Reyes <jonamreyes@gmail.com>
2018-08-18 11:14:00 -0400

    added clarifai api

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation.js'
 import Logo from './components/Logo/Logo.js'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js'
+import FaceRecognition from  './components/FaceRecognition/FaceRecognition.js'
 import Rank from './components/Rank/Rank.js'
 import './App.css';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+
+// install the npm version of clarifai
+const app = new Clarifai.App({
+  apiKey: '41bc4a5ace4b466781b5eef67d5ea78f'
+})
 
 
 const particlesOptions = {
@@ -21,21 +28,38 @@ const particlesOptions = {
 
 class App extends Component {
   
-  //detect user input
+  //detect user input - STATES
   //create a state so that the app knows what the user enters
   constructor(){
     super();
     this.state = {
       input: '',
+      imageUrl: ''
     }
   }
-
+  
+  //event listerners with states
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({input: event.target.value});
   }
-
   onButtonSubmit = () => {
-    console.log('click');
+    //then pass this on to the facerecognition
+    this.setState ({imageUrl: this.state.input});
+    
+    // CLARIFAI API 
+    app.models.predict(
+      Clarifai.FACE_DETECT_MODEL, 
+      this.state.input)
+    .then(
+    function(response) {
+        // do something with response
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+      },
+      function(err) {
+       // there was an error
+       console.log(err);
+      }
+    );
   }
 
   render() {
@@ -53,10 +77,15 @@ class App extends Component {
           onInputChange={this.onInputChange} 
           onButtonSubmit={this.onButtonSubmit}
         />     
-
-        { /* 
-        <FaceRecognition /> */
+        { 
+        /*add face recognition function using Clarifai Face Recognition API 
+          display the image under the form
+        */
         }
+
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
+        
+        
       </div>
     );
   }
