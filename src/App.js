004be9d8ea1d770d71974bacd010be59379c5b83
Jona Reyes <jonamreyes@gmail.com>
2018-08-18 13:43:18 -0400

    fixed sign out routing

--- src/App.js
+++ src/App.js
@@ -39,7 +39,8 @@ class App extends Component {
       input: '',
       imageUrl: '', 
       box: {},
-      route: 'signin'
+      route: 'signin',
+      isSignedIn: false
     }
   }
   
@@ -68,7 +69,12 @@ class App extends Component {
   //event listerners with states
 
   onRouteChange = (route) => {
-    this.setState ({route: route});
+    if (route === 'signout'){
+      this.setState ({isSignedIn: false});
+    } else if (route === 'home') {
+      this.setState ({isSignedIn: true});
+    }
+    this.setState({route: route});
   }
 
   onInputChange = (event) => {
@@ -93,7 +99,7 @@ class App extends Component {
           params={particlesOptions}
         />
 
-        <Navigation onRouteChange={this.onRouteChange} />
+        <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange} />
         
         {/* if statement if signed in or not */ }
         
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,14 +1,32 @@
 import React from 'react';
 
-const Navigation = ({onRouteChange}) => {
-	return (
-		<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-		<p 
-		onClick={() => onRouteChange('signin')} 
-		className='fs link dim black underline pa3 pointer' 
-		style={{color: 'white', textDecoration: 'none', fontSize: '25px'}}> Sign Out </p>
-		</nav>
+const Navigation = ({onRouteChange, isSignedIn}) => {
+	if (isSignedIn){
+		return (
+			<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+				<p 
+					onClick={() => onRouteChange('signout')} 
+					className='fs link dim black underline pa3 pointer' 
+					style={{color: 'white', textDecoration: 'none', fontSize: '25px'}}> Sign Out 
+				</p>
+			</nav>
 		);
+	} else {
+		return (
+			<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+				<p 
+					onClick={() => onRouteChange('signin')} 
+					className='fs link dim black underline pa3 pointer' 
+					style={{color: 'white', textDecoration: 'none', fontSize: '25px'}}> Sign In
+				</p>
+				<p 
+					onClick={() => onRouteChange('register')} 
+					className='fs link dim black underline pa3 pointer' 
+					style={{color: 'white', textDecoration: 'none', fontSize: '25px'}}> Register
+				</p>
+			</nav>
+		);
+	}	
 }
 
 export default Navigation;
